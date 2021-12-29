e009e0984a81485f65895b8e4c05b60154979a40
derBobo <derBobo@users.noreply.github.com>
2021-12-29 17:24:40 +0100

    added day 17 part 2

--- 2021/d17/src/main.rs
+++ 2021/d17/src/main.rs
@@ -5,7 +5,7 @@ use std::io::{BufRead, BufReader};
 use std::thread::current;
 
 fn main() {
-    let path = "SAMPLEINPUT17.txt";
+    let path = "INPUT17.txt";
     let input = File::open(path).expect("file not found!");
     let buffered = BufReader::new(input);
     let lines = buffered
@@ -28,8 +28,11 @@ fn main() {
     println!("{:?}", y_range);
     let mut current_step = 1;
     let mut max_height = 0;
-    let mut avilable_velocities = 0;
+    let mut avilable_velocities = HashSet::new();
     loop {
+        if current_step == 4 || current_step == 5 {
+            let c = 0;
+        }
         //calculate intervall of start y velocities that would reach the area
         let lower_y_bound =
             (2.0 * y_range[0] / current_step as f64 + (current_step - 1) as f64) / 2.0;
@@ -42,35 +45,35 @@ fn main() {
         //calculate intervall of start x velocities that would reach the area
         // x bound only work if target area values>0
         let mut lower_x_bound =
-            (2.0 * x_range[0] / current_step as f64 + (current_step - 1) as f64) / 2.0;
-        let upper_x_bound =
-            (2.0 * x_range[1] / current_step as f64 + (current_step - 1) as f64) / 2.0;
+            ((2.0 * x_range[0] / current_step as f64 + (current_step - 1) as f64) / 2.0).ceil()
+                as i64;
+        let upper_x_bound = ((2.0 * x_range[1] / current_step as f64 + (current_step - 1) as f64)
+            / 2.0)
+            .floor() as i64;
 
-        if lower_x_bound < current_step as f64 {
-            lower_x_bound = current_step as f64;
+        if lower_x_bound < current_step {
+            lower_x_bound = current_step;
         }
 
-        let number_of_y_velocities =
-            (lower_y_bound.ceil() as i64..=upper_y_bound.floor() as i64).count();
-        let mut number_of_x_velocities =
-            (lower_x_bound.ceil() as i64..=upper_x_bound.floor() as i64).count();
+        for y in lower_y_bound.ceil() as i64..=upper_y_bound.floor() as i64 {
+            for x in lower_x_bound..=upper_x_bound {
+                avilable_velocities.insert((x, y));
+            }
+        }
 
-        // calculate x velocities that would hit but already slowed down to 0
-        if number_of_y_velocities > 0
-            && x_range[0] < ((current_step * current_step + current_step) / 2) as f64
-        {
-            let lower_limit = ((8.0 * x_range[0] + 1.0).sqrt() - 1.0) / 2.0;
-            let mut upper_limit = ((8.0 * x_range[1] + 1.0).sqrt() - 1.0) / 2.0;
+        // calculate x velocities that would hit bc they already slowed down to 0
+        if x_range[0] < ((current_step * current_step + current_step) / 2) as f64 {
+            let lower_limit = (((8.0 * x_range[0] + 1.0).sqrt() - 1.0) / 2.0).ceil() as i64;
+            let mut upper_limit = (((8.0 * x_range[1] + 1.0).sqrt() - 1.0) / 2.0).floor() as i64;
             if x_range[1] > ((current_step * current_step + current_step) / 2) as f64 {
-                upper_limit = current_step as f64;
+                upper_limit = current_step;
             }
-            if upper_limit.floor() as i64 == lower_x_bound.ceil() as i64 {
-                upper_limit -= 1.0;
+            for y in lower_y_bound.ceil() as i64..=upper_y_bound.floor() as i64 {
+                for x in lower_limit..=upper_limit {
+                    avilable_velocities.insert((x, y));
+                }
             }
-            number_of_x_velocities +=
-                (lower_limit.ceil() as i64..=upper_limit.floor() as i64).count();
         }
-        avilable_velocities += number_of_x_velocities * number_of_y_velocities;
 
         //calculate part1
         for i in lower_y_bound.ceil() as i64..=upper_y_bound.floor() as i64 {
@@ -88,5 +91,5 @@ fn main() {
         current_step += 1;
     }
     println!("{:?}", max_height);
-    println!("{}", avilable_velocities);
+    println!("{}", avilable_velocities.len());
 }
